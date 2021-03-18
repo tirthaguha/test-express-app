b7fe7bb434d960ad187888539a2c9e34d917dbc7
Tirtha Guha <tirtha.guha@gmail.com>
2021-03-18 22:30:36 +0530

    chore: Added helmet, cors, dotenv and got

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+if (!process.env.NODE_ENV) {
+  require("dotenv").config();
+}
+
 var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var nocache = require("nocache");
+var helmet = require("helmet");
+var cors = require("cors");
 
 var usersRouter = require("./routes/users");
 
@@ -10,6 +17,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(nocache());
+app.use(helmet.xssFilter());
+app.use(helmet.referrerPolicy({ policy: "same-origin" }));
+app.use(cors());
 
 app.use("/users", usersRouter);
 
